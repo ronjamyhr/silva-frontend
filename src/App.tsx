de44462d22851f86e758bac588573c455450d332
Fredrik Skogberg <skogberg.fredrik@gmail.com>
2019-08-29 13:32:44 +0200

    add page not found and router to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
 import Contact from './components/contact/Contact'
+import NotFound from './components/not-found/Not-found';
 
 class App extends React.Component {
 	public render() {
@@ -12,14 +13,15 @@ class App extends React.Component {
 		    <div className="App">
 				<Router>
 					<Navbar />
-					<Route exact path="/" render={props => (
-						<React.Fragment>
-							<Home />
-						</React.Fragment>
-					)}></Route>
-					<Route path="/contact" component={Contact}>
-
-					</Route>
+					<Switch>
+						<Route exact path="/" render={props => (
+							<React.Fragment>
+								<Home />
+							</React.Fragment>
+						)}></Route>
+						<Route path="/contact" component={Contact} />
+						<Route component={ NotFound } />
+					</Switch>
 				</Router>
 				<Footer />
 		    </div>
---
+++ src/components/not-found/Not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Not-found.css';
+
+class NotFound extends React.Component {
+	public render() {
+		return (
+			<div>
+			  	Not found
+			</div>
+		);
+	}
+}
+
+export default NotFound;
