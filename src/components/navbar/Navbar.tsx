7c5fa74f166d64e54df9686a903ea17422684eb9
ronjamyhr <ronja_myhr@hotmail.com>
2019-09-02 08:49:44 +0200

    add content and styling in nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,13 +5,19 @@ import { Link } from 'react-router-dom'
 class Navbar extends React.Component {
 	public render() {
 		return (
-			<div>
-			  	<nav>
-					<Link to="/">Home</Link> |
-					<Link to="/booking">Book</Link> |
-					<Link to="/contact">Contact</Link> |
-			 	</nav>
-			</div>
+			<React.Fragment>
+				<nav className="navbar">
+					<div className="medium-logo">
+						<img className="medium-logo" src={require("./../../images/mediumLogo.png")} alt="Logotype of the restaurant"
+							title="Silva logo" />
+					</div>
+					<ul className="navigation">
+						<li><Link className="links" to="/">Hem</Link></li>
+						<li><Link className="links" to="/booking">Boka bord</Link></li>
+						<li><Link className="links" to="/contact">Kontakt</Link></li>
+					</ul>
+				</nav>
+			</React.Fragment>
 		);
 	}
 }
