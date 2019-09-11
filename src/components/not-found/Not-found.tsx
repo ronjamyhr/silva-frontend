64da8d7b464c859d8903617d993790616d4eaa95
ronjamyhr <ronja_myhr@hotmail.com>
2019-09-11 15:29:30 +0200

    add content and style

--- src/components/not-found/Not-found.tsx
+++ src/components/not-found/Not-found.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Not-found.css';
+import { Link } from 'react-router-dom';
 
 class NotFound extends React.Component {
 	public render() {
 		return (
-			<div>
-			  	<p>Not found</p>
-			</div>
+			<React.Fragment>
+				<div className="not-found-container">
+					<div className="not-found-icon-container"><i className="fas fa-exclamation not-found-icon"></i></div>
+					<div className="not-found-text-container">
+						<p className="not-found-text">Sidan du letar efter kunde inte hittas!</p>
+						<Link className="not-found-home-link" to="/">Gå till startsidan</Link>
+					</div>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
